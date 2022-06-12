df7c78f7448ccb695e6a14d15b99d3e9545784e0
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-06-12 23:46:36 +0530

    [Tushar Vidwans] TC1.2 = Testing MoodAnalyser for Happy Response Mood

--- TestMoodAnalyser/MoodAnaylyser_Test.cs
+++ TestMoodAnalyser/MoodAnaylyser_Test.cs
@@ -24,5 +24,25 @@ namespace TestMoodAnalyser
             Assert.AreEqual(expected_Output, actual_Output);
 
         }
+
+        [TestMethod]
+        public void AnalyseMood_HappyTest()
+        {
+
+            //Arrange
+            string message = "I am in Any Mood.";
+            string expected_Output = "Happy";
+            string actual_Output;
+
+            MoodAnalyser moodAnaylyser = new MoodAnalyser();
+
+            //Act
+
+            actual_Output = moodAnaylyser.AnalyseMood(message.ToLower());
+
+            //Assert
+            Assert.AreEqual(expected_Output, actual_Output);
+
+        }
     }
 }
