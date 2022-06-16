868c784bd9f95472cfd9c2eeeb069ae1435d7349
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-06-16 16:06:16 +0530

    [Tushar Vidwans] Using reflection to create MoodAnalyser

---
+++ MoodAnalyser_Day20/MoodAnalysisCustomException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyser_Day20
+{
+    public class MoodAnalysisCustomException : Exception
+    {
+        public enum ExceptionType
+        {
+            NULL_MESSAGE, EMPTY_MESSAGE,
+            NO_SUCH_FIELD, NO_SUCH_METHOD,
+            NO_SUCH_CLASS, OBJECT_CREATION_ISSUE
+        }
+
+        private readonly ExceptionType type;
+
+
+        public MoodAnalysisCustomException(ExceptionType type, string message)
+        {
+            this.type = type;
+        }
+    }
+}
