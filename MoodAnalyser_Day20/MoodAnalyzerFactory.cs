15509d43c3f04b53f33d962b05571a42fdb6bb03
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-06-16 19:43:29 +0530

    [Tushar Vidwans] Using reflection with parameter constructor

--- MoodAnalyser_Day20/MoodAnalyzerFactory.cs
+++ MoodAnalyser_Day20/MoodAnalyzerFactory.cs
@@ -11,6 +11,51 @@ namespace MoodAnalyser_Day20
     public class MoodAnalyzerFactory
     {
 
+        /// <summary>
+        /// UC 5 - For parameterised constructor by passing message parameter to the class method.
+        /// </summary>
+        /// <param name="className"></param>
+        /// <param name="constructorName"></param>
+        /// <returns></returns>
+        /// <exception cref="MoodAnalysisCustomException"></exception>
+
+        public static object CreateMoodAnalyseUsingParameterizedConstructor(string className, string constructorName, string message)
+        {
+            Type type = typeof(MoodAnalyser);
+
+            if(type.Name.Equals(className) || type.FullName.Equals(className))
+            {
+                if (type.Name.Equals(constructorName))
+                {
+                    ConstructorInfo ctor = type.GetConstructor(new[] {typeof(string)});
+                    object instance = ctor.Invoke(new object[] { "Happy" });
+                    return instance;
+                }
+                else
+                {
+                    throw new MoodAnalysisCustomException(MoodAnalysisCustomException.ExceptionType.NO_SUCH_METHOD, "Constructor is not found");
+                }
+            }
+            else
+            {
+                throw new MoodAnalysisCustomException(MoodAnalysisCustomException.ExceptionType.NO_SUCH_CLASS, "Class not found");
+            }
+        }
+
+
+
+
+
+
+
+        /// <summary>
+        /// CreateMoodAnalyse method to create object of MoodAnalyse class. UC 4
+        /// </summary>
+        /// <param name="className"></param>
+        /// <param name="constructorName"></param>
+        /// <returns></returns>
+        /// <exception cref="MoodAnalysisCustomException"></exception>
+
         public static object CreateMoodAnalysis(string className, string constructorName)
         {
             string pattern = @"."+ constructorName +"$";
